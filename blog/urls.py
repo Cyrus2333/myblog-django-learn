e2905015cf24f463eac15c143a684b023f9b4d3a
cyrus <476472389@qq.com>
2019-03-19 13:06:04 +0800

    增加博客主页和文章页页面

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path("<int:article_id>",views.article_page)
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from . import models
 
 def index(request):
-    article = models.Article.objects.get(pk=1)
-    return render(request,"blog/index.html",{"article": article})
-    
+    articles = models.Article.objects.all()
+    return render(request,"blog/index.html",{"articles": articles})  
+
+def article_page(request,article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request,"blog/article_page.html",{"article":article})
