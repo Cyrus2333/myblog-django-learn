b98958813d97d4b50f11eff7ec218e5d6affc06c
cyrus <476472389@qq.com>
2019-03-19 19:27:51 +0800

    增加发布新文章的功能

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ app_name ='blog'
 
 urlpatterns = [
     path('', views.index),
-    path("<int:article_id>",views.article_page,name="article_page")
+    path("<int:article_id>",views.article_page,name="article_page"),
+    path("newarticle",views.new_article,name="new_article"),
+    path("publish",views.publish,name="publish")
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,13 @@ def index(request):
 def article_page(request,article_id):
     article = models.Article.objects.get(pk=article_id)
     return render(request,"blog/article_page.html",{"article":article})
+
+def new_article(request):
+    return render(request,"blog/new_article.html")
+
+def publish(request):
+    title = request.POST.get("title","没有标题")
+    content = request.POST.get("content","没有内容")
+    models.Article.objects.create(title=title,content=content)
+    articles = models.Article.objects.all()
+    return render(request,"blog/index.html",{"articles": articles})  
