3a956fde54b94a0fefd376b11ea2a5a01005889f
cyrus <476472389@qq.com>
2019-03-19 13:23:20 +0800

    增加超链接的配置

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name ='blog' 
+
 urlpatterns = [
     path('', views.index),
-    path("<int:article_id>",views.article_page)
+    path("<int:article_id>",views.article_page,name="article_page")
 ]
