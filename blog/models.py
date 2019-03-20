becc196ad44946ca67e5ae8aabb843da71d20c78
cyrus <476472389@qq.com>
2019-03-20 19:59:35 +0800

    增加template过滤，完善admin功能；

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=32, default="还没想好标题…")
     content = models.TextField()
+    pub_time = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
